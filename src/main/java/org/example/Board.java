428e8984bc1afb7b653ee53ece0d0cbc94a620cb
may-leth <mayleth9609@gmail.com>
2025-05-19 15:24:16 +0200

    feat(Board): add imethods isFull and isWinner

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -39,14 +39,35 @@ public class Board {
         System.out.println("test updateLastMove");
     }
 
-    public boolean isWinner () {
-
-        System.out.println("test isWinner");
+    public boolean isWinner (Player player) {
+        char token = player.getToken();
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == token && board[i][1] == token && board[i][2] == token) {
+                return true;
+            }
+            if (board[0][i] == token && board[1][i] == token && board[2][i] == token) {
+                return true;
+            }
+        }
+        if (board[0][0] == token && board[1][1] == token && board[2][2] == token){
+            return true;
+        }
+        if (board[0][2] == token && board[1][1] == token && board[2][0] == token){
+            return true;
+        }
+        System.out.println("no hay ganador");
         return false;
     }
 
     public boolean isFull() {
-        System.out.println("test isFull");
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (this.board[row][col] == '_') {
+                    return false;
+                }
+            }
+        }
+        System.out.println("si esta lleno es true");
         return true;
     }
 
